c0fd38cadc46151b26b049cec4777282804f7865
quro <hs970216@naver.com>
2025-01-30 03:36:16 +0900

    feat: CORS 설정 수정

--- src/main/java/projects/kickConnect/config/WebConfig.java
+++ src/main/java/projects/kickConnect/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://kick-connect-nine.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
 }
